6e014f56e8652a29d837ec3d85f8c6dafd9ca494
bena-sudo <joanbenaventcastells@gmail.com>
2025-02-12 22:26:53 +0100

    Add sql database

---
+++ sql/database.sql
@@ -0,0 +1,22 @@
+CREATE USER videojuegos WIDH PASSWORD 'videojuegos';
+CREATE DATABASE videojuegos OWNER 'videojuegos';
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    rol VARCHAR(50) NOT NULL CHECK (rol IN ('admin', 'cliente'))
+);
+CREATE TABLE videojuegos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    imagen_base64 TEXT
+);
+CREATE TABLE compras (
+    id SERIAL PRIMARY KEY,
+    usuario_id INT REFERENCES usuarios(id) ON DELETE CASCADE,
+    videojuego_id INT REFERENCES videojuegos(id) ON DELETE CASCADE,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
