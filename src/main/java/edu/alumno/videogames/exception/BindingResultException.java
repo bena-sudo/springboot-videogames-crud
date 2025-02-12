5ea3cc80e5cdcba826183e06f71d6bac76058efd
bena-sudo <joanbenaventcastells@gmail.com>
2025-02-13 00:33:25 +0100

    Add id exception

--- src/main/java/edu/alumno/videogames/exception/BindingResultException.java
+++ src/main/java/edu/alumno/videogames/exception/BindingResultException.java
@@ -1,11 +1,14 @@
 package edu.alumno.videogames.exception;
 
 import java.util.Map;
+
 import lombok.Getter;
 
 @Getter
 public class BindingResultException extends RuntimeException {
-    
+
+    private static final long serialVersionUID = 1L;
+
     private final String errorCode;
 
     private final Map<String, String> validationErrors;
---
+++ src/main/java/edu/alumno/videogames/model/IdEntityLong.java
@@ -0,0 +1,18 @@
+package edu.alumno.videogames.model;
+
+import edu.alumno.videogames.exception.DataValidationException;
+import lombok.Value;
+
+@Value
+public class IdEntityLong {
+    Long value;
+
+    public IdEntityLong(String id) {
+        try {
+            this.value = Long.valueOf(id);
+        } catch (NumberFormatException ex) {
+            throw new DataValidationException("ID_FORMAT_INVALID",
+                    " El ID debe ser un valor numérico de tipo Long (INT64).");
+        }
+    }
+}
